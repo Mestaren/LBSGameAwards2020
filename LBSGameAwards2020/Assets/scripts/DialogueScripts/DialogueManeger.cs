15ddb91f8cc9b28103f08cac15fd44e48b65d68a
Mestaren <lucas.svensson@elev.ga.lbs.se>
2021-04-28 12:13:17 +0200

    fortsatt med dialogue och fortsatt med map struktur

---
+++ LBSGameAwards2020/Assets/scripts/DialogueScripts/DialogueManeger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManeger : MonoBehaviour
+{
+    private Queue<string> Sentences;
+
+
+    void Start()
+    {
+        Sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("starting conversation with " + dialogue.name);
+
+        Sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            Sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (Sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = Sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+    void EndDialogue()
+    {
+        Debug.Log("end conversation");
+    }
+}
---
+++ LBSGameAwards2020/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 20f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
