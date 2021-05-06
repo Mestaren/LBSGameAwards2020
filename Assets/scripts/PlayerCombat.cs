e687fd82fa25b23a781dba213d28d91d909956e5
Mestaren <lucas.svensson@elev.ga.lbs.se>
2021-05-06 09:05:00 +0200

    Test??

---
+++ Assets/scripts/PlayerCombat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //if statement som registrerar om jag trycker ner space knappen
+        {
+            Attack(); //kallar på funktionen attack varje gång jag trycker space 
+        }
+    }
+
+
+    void Attack()
+    {
+        //spela en attack animation
+        animator.SetTrigger("Attack down");
+
+
+
+    }
+}
